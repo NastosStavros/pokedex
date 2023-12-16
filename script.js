ff41c8c9880ca30afd684633323cc4385b14c457
NastosStavros <n.stavros@outlook.de>
2023-12-16 14:04:42 +0100

    functions

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+let allPokemon = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon',
+'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie',
+'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey',
+'pidgeotto', 'pidgeot', 'rattata', 'raticate']
 let currentPokemon;
 
+
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    console.log(currentPokemon);
-    renderPokemonInfo();
+    for (let i = 0; i < allPokemon.length; i++) {
+        let pokemon = allPokemon[i];
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        console.log(currentPokemon);
+        createPokemonCard(pokemon);
+    }
 }
 
-function renderPokemonInfo() {
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+function createPokemonCard(pokemon) {
+    let pokedex = document.getElementById('pokedex');
+    pokedex.innerHTML += `<div id="pokedex-card-${pokemon}" class="card" style="width: 18rem;">
+    <img id="pokemonImage${pokemon}" src="" class="card-img-top">
+    <div class="card-body">
+        <h5 id="pokemonName-${pokemon}" class="card-title"></h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+</div>`;
+showPokemonInfo(pokemon);
+}
+
+function showPokemonInfo(pokemon) {
+    let pokemonName = document.getElementById(`pokemonName-${pokemon}`);
+    let pokemonImage = document.getElementById(`pokemonImage${pokemon}`);
+    pokemonName.innerHTML = currentPokemon['name'];
+    pokemonImage.src = currentPokemon['sprites']['front_default'];
 }
