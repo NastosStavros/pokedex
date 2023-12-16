8f8a74c7ee0451f8840f72581f0b3568d8a4fc3d
NastosStavros <n.stavros@outlook.de>
2023-12-16 15:34:41 +0100

    colors

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let allPokemon = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon',
 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie',
 'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey',
-'pidgeotto', 'pidgeot', 'rattata', 'raticate']
+'pidgeotto', 'pidgeot', 'rattata', 'raticate', 'spearow', 'fearow',
+'ekans', 'arbok', 'pikachu', 'raichu', 'sandshrew', 'sandslash',
+'nidoran-f', 'nidorina', 'nidoqueen', 'nidoran-m', 'nidorino', 'nidoking',
+'clefairy', 'clefable', 'vulpix', 'ninetales', 'jigglypuff', 'wigglytuff',
+'zubat', 'golbat', 'oddish', 'gloom', 'vileplume', 'paras', 'parasect',
+'venonat', 'venomoth']
 let currentPokemon;
 
 
@@ -13,6 +18,7 @@ async function loadPokemon() {
         currentPokemon = await response.json();
         console.log(currentPokemon);
         createPokemonCard(pokemon);
+        
     }
 }
 
@@ -22,15 +28,50 @@ function createPokemonCard(pokemon) {
     <img id="pokemonImage${pokemon}" src="" class="card-img-top">
     <div class="card-body">
         <h5 id="pokemonName-${pokemon}" class="card-title"></h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p id="pokemonType${pokemon}" class="card-text"></p>
     </div>
 </div>`;
 showPokemonInfo(pokemon);
+
 }
 
 function showPokemonInfo(pokemon) {
     let pokemonName = document.getElementById(`pokemonName-${pokemon}`);
     let pokemonImage = document.getElementById(`pokemonImage${pokemon}`);
+    let pokemonType = document.getElementById(`pokemonType${pokemon}`);
+    let pokemonTypeInfo = currentPokemon['types'][0]['type']['name'];
     pokemonName.innerHTML = currentPokemon['name'];
     pokemonImage.src = currentPokemon['sprites']['front_default'];
+    pokemonType.innerHTML ="Type : " +pokemonTypeInfo;
+    showBgbyType(pokemonType, pokemonTypeInfo);
+}
+
+function showBgbyType(pokemonType, pokemonTypeInfo) {
+    if (pokemonTypeInfo === 'fire') {
+        pokemonType.classList.add('bg-fire');
+    }
+    if (pokemonTypeInfo === 'grass') {
+        pokemonType.classList.add('bg-grass');
+    }
+    if (pokemonTypeInfo === 'water') {
+        pokemonType.classList.add('bg-water');
+    }
+    if (pokemonTypeInfo === 'normal') {
+        pokemonType.classList.add('bg-normal');
+    }
+    if (pokemonTypeInfo === 'poison') {
+        pokemonType.classList.add('bg-poison');
+    }
+    if (pokemonTypeInfo === 'ground') {
+        pokemonType.classList.add('bg-ground');
+    }
+    if (pokemonTypeInfo === 'electric') {
+        pokemonType.classList.add('bg-electric');
+    }
+    if (pokemonTypeInfo === 'bug') {
+        pokemonType.classList.add('bg-bug');
+    }
+    if (pokemonTypeInfo === 'fairy') {
+        pokemonType.classList.add('bg-fairy');
+    } 
 }
